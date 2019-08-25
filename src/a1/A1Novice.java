d350043228677b714e0e080b231e87b474e60950
kedaek <54493000+kedaek@users.noreply.github.com>
2019-08-25 14:28:42 -0400

    Update A1Novice.java

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,31 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int numbCustomer = 0;
+		String firstName;
+		String lastName;
+		int itemTypes;
+		int itemQuant;
+		String itemName;
+		double itemPrice;
 		
+		System.out.println("How many customers?");
+		numbCustomer = scan.nextInt();
+		
+		while(numbCustomer > 0) {
+			firstName = scan.next();
+			lastName = scan.next();
+			itemTypes = scan.nextInt();
+			double totalPrice = 0;
+				while(itemTypes > 0) {
+					itemQuant = scan.nextInt();
+					itemName = scan.next();
+					itemPrice = scan.nextDouble();
+					totalPrice = itemQuant * itemPrice + totalPrice;
+					itemTypes--;
+				}
+			System.out.println(firstname.charAt(0) + ". " lastName + " " + totalPrice);
+		numbCustomer--;
+		}
 	}
 }
